b6db756b1cd6ab0797f670551cb0cb60f459dad5
Alvantesha Priliandi <alvanteshapriliandi@192.168.5.254>
2025-02-14 16:23:45 +0700

    finish pertemuan 1

--- src/views/pages/pertemuan1/tugas/Tugas.vue
+++ src/views/pages/pertemuan1/tugas/Tugas.vue
@@ -17,8 +17,23 @@
 
 <script setup>
 
-    import { ref } from 'vue';
+    import { ref, computed } from 'vue';
     import ProductCard from '../../../../components/ProductCard.vue';
+
+    const newTask = ref('')
+    const tasks = ref([])
+    const total = ref(0)
+
+    const addTask = () => {
+        if (newTask.value.trimEnd() !== '') {
+            tasks.value.push(newTask.value)
+            newTask.value = ''
+        }
+    }
+
+    const discount = computed(() => {
+        return total.value > 1000000 ? total.value * 0.1 : 0
+    })
 
 </script>
 
@@ -31,13 +46,36 @@
     </div> -->
 
     <!-- Start No Tugas 2 -->
-    <hr>
+    <!-- <hr>
     <div>
 
         <h2>Product Card:</h2>
         <ProductCard name="Laptop Gaming" :price="10000" />
         <ProductCard name="SmartPhone Kitakita" :price="8000000" />
 
+    </div> -->
+
+    <!-- Start No Tugas 3 -->
+    <!-- <hr>
+    <div>
+        <h2>Tugas 3 - Menggunakan Reactive State</h2>
+        <input v-model="newTask" placeholder="Masukkan tugas..." />
+        <button @click="addTask"> Tambah </button>
+
+        <ul>
+            <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+        </ul>
+    </div> -->
+    
+    <!-- Start No Tugas 4 -->
+    <hr>
+    <div>
+        <h2>Tugas 4 - Menggunakan Computed Properties</h2>
+        <p>Total Belanja: Rp {{ total }}</p>
+        <p>Diskon: Rp {{ discount }}</p>
+
+        <button @click="total += 500000">Tambah Rp 500.000</button>
     </div>
+
 
 </template>
